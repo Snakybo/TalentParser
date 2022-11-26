1a93c6db85e88536e60a3d3ca31b9d102979c5b5
Kevin Krol <krol.kevin@gmail.com>
2022-11-26 15:11:28 +0100

    Use different Lua library, include more data in talent dump

--- parse.py
+++ parse.py
@@ -1,9 +1,15 @@
-from luaparser import ast
-from luaparser import astnodes
+import luadata
 
 class TalentData:
 	index: int
-	talentId: int
+	entryID: int
+	spellID: int
+	name: str
+	icon: int
+
+class PvPTalentData:
+	index: int
+	talentID: int
 	name: str
 
 class SpecData:
@@ -12,14 +18,14 @@ class SpecData:
 	specName: str
 	className: str
 	classFileName: str
-	build: str
+	lastUpdateBuild: str
 	talents: list[TalentData]
-	pvpTalents: list[list[TalentData]]
+	pvpTalents: list[PvPTalentData]
 
 	def talents_to_string(self):
 		result: str = ""
 		result += "\t-- " + self.specName + " " + self.className + "\n"
-		result += "\t[" + str(self.specId) + "] = {\n"
+		result += "\t[" + str(self.specID) + "] = {\n"
 
 		for i in range(len(self.talents)):
 			result += "\t\t" + self.talent_row_to_string(i) + "\n"
@@ -29,33 +35,30 @@ class SpecData:
 		return result
 
 	def talent_row_to_string(self, index):
-		result: str = ""
-		result += str(self.talents[index].talentId) + ", "
-		result += "-- "
-		result += "[" + str(index) + "] "
-		result += str(self.talents[index].name)
+		result: str = "{ "
+		result += "entryID = " + str(self.talents[index].entryID) + ", "
+		result += "spellID = " + str(self.talents[index].spellID) + ", "
+		result += "name = \"" + str(self.talents[index].name) + "\", "
+		result += "icon = " + str(self.talents[index].icon) + " },"
 
 		return result
 
 	def pvp_talents_to_string(self):
 		result: str = ""
 		result += "\t-- " + self.specName + " " + self.className + "\n"
-		result += "\t[" + str(self.specId) + "] = {\n"
-
-		for i in range(len(self.pvpTalents)):
-			result += "\t\t[" + str(i + 1) +"] = " + self.pvp_talent_row_to_string(self.pvpTalents[i]) + "\n"
-
-		result += "\t},\n"
+		result += "\t[" + str(self.specID) + "] = "
+		result += self.pvp_talent_row_to_string(self.pvpTalents)
+		result += "\n"
 
 		return result
 
-	def pvp_talent_row_to_string(self, row: list[TalentData]):
+	def pvp_talent_row_to_string(self, row: list[PvPTalentData]):
 		result: str = ""
 		result += "{ "
 
 		if len(row) > 0:
 			for talent in row:
-				result += str(talent.talentId) + ", "
+				result += str(talent.talentID) + ", "
 
 			result += "}, -- "
 
@@ -69,101 +72,47 @@ class SpecData:
 
 		return result
 
-def parse_spec_data(fields: list[ast.Field]):
+def parse_spec_data(lua):
 	data = SpecData()
-
-	for field in fields:
-		key = field.key.s
-
-		if key == "specID":
-			data.specId = field.value.n
-		if key == "specIndex":
-			data.specIndex = field.value.n
-		if key == "lastUpdateBuild":
-			data.build = field.value.s
-		elif key == "specName":
-			data.specName = field.value.s
-		elif key == "className":
-			data.className = field.value.s
-		elif key == "classFileName":
-			data.classFileName = field.value.s
-		elif key == "talents":
-			data.talents = parse_talent_table(field.value.fields)
-		elif key == "pvpTalents":
-		 	data.pvpTalents = parse_pvp_talent_table(field.value.fields)
+	data.specID = lua["specID"]
+	data.specIndex = lua["specIndex"]
+	data.lastUpdateBuild = lua["lastUpdateBuild"]
+	data.specName = lua["specName"]
+	data.className = lua["className"]
+	data.classFileName = lua["classFileName"]
+	data.talents = parse_talents(lua["talents"])
+	data.pvpTalents = parse_pvp_talents(lua["pvpTalents"])
 
 	return data
 
-def parse_pvp_talent_table(fields: list[astnodes.Field]):
-	result: list[list[TalentData]] = []
+def parse_pvp_talents(lua):
+	result: list[PvPTalentData] = []
 
-	for field in fields:
-		index = field.key.n - 1
+	for talent in lua:
+		data = PvPTalentData()
+		data.index = len(result)
+		data.talentID = talent["talentID"]
+		data.name = talent["name"]
 
-		try:
-			result[index] = parse_pvp_talents(field.value.fields)
-		except IndexError:
-			for _ in range(index - len(result) + 1):
-				result.append(None)
-			result[index] = parse_pvp_talents(field.value.fields)
-
-	return result
-
-def parse_talent_table(trees: list[astnodes.Field]):
-	result: list[TalentData] = []
-
-	for tree in trees:
-		parse_talent_node(tree.value.fields, result)
-
-	return result
-
-def parse_talent_node(fields: list[astnodes.Field], result: list[TalentData]):
-	for field in fields:
-		key = field.key.s
-
-		if key == "entryIDs":
-			parse_talent_node_entries(field.value.fields, result)
-
-def parse_talent_node_entries(entries: list[astnodes.Field], result: list[TalentData]):
-	for entry in entries:
-		data = TalentData()
-		parse_talent_node_entry(entry.value.fields, data)
 		result.append(data)
 
-def parse_talent_node_entry(fields: list[astnodes.Field], data: TalentData):
-	for field in fields:
-		key = field.key.s
-
-		if key == "spellID":
-			data.talentId = field.value.n
-		elif key == "name":
-			data.name = field.value.s
-
-def parse_pvp_talents(fields: list[astnodes.Field]):
-	result: list[TalentData] = []
-
-	for field in fields:
-		data = TalentData()
-		data.index = field.key.n - 1
-		parse_pvp_talent(field.value.fields, data)
-
-		try:
-			result[data.index] = data
-		except IndexError:
-			for _ in range(data.index - len(result) + 1):
-				result.append(None)
-			result[data.index] = data
-
 	return result
 
-def parse_pvp_talent(fields: list[astnodes.Field], data: TalentData):
-	for field in fields:
-		key = field.key.s
+def parse_talents(lua):
+	result: list[TalentData] = []
 
-		if key == "talentID":
-			data.talentId = field.value.n
-		elif key == "name":
-			data.name = field.value.s
+	for talent in lua:
+		for entryID in talent["entryIDs"]:
+			data = TalentData()
+			data.index = len(result)
+			data.entryID = entryID
+			data.spellID = talent["entries"][entryID]["definition"]["spellID"]
+			data.name = talent["entries"][entryID]["definition"]["name"]
+			data.icon = talent["entries"][entryID]["definition"]["icon"]
+
+			result.append(data)
+
+	return result
 
 def generate_spec_list(specs: list[SpecData]):
 	result: dict[str,list[SpecData]] = {}
@@ -206,19 +155,19 @@ def generate_lua_table(build: str, specs: list[SpecData]):
 			if s is None:
 				continue
 
-			result += "\t\t[" + str(s.specIndex) + "] = " + str(s.specId) + ", -- " + s.specName + "\n"
+			result += "\t\t[" + str(s.specIndex) + "] = " + str(s.specID) + ", -- " + s.specName + "\n"
 
 		result += "\t},\n"
 
 	result += "}\n\n"
-	result += "--- @type table<integer,integer[]>\n"
+	result += "--- @type table<integer,TalentEntry[]>\n"
 	result += "local talents = {\n"
 
 	for spec in specs:
 		result += spec.talents_to_string()
 
 	result += "}\n\n"
-	result += "--- @type table<integer,table<integer,integer[]>>\n"
+	result += "--- @type table<integer,integer[]>\n"
 	result += "local pvpTalents = {\n"
 
 	for spec in specs:
@@ -230,34 +179,25 @@ def generate_lua_table(build: str, specs: list[SpecData]):
 	result += "\tversion = version,\n"
 	result += "\tspecializations = specializations,\n"
 	result += "\ttalents = talents,\n"
-	result += "\tpvpTalentSlotCount = 3,\n"
 	result += "\tpvpTalents = pvpTalents\n"
 	result += "})\n"
 
 	return result
 
 def get_build(data: SpecData):
-	return data.build
+	return data.lastUpdateBuild
 
 def get_spec_id(data: SpecData):
-	return data.specId
+	return data.specID
 
-try:
-	fs = open("TalentExtractor.lua", "r", encoding="utf8")
-	src = fs.read()
-	fs.close
-except Exception as fserr:
-	print("failed to read file \"TalentExtractor.lua\": " + str(fserr))
-	exit(1)
-
-tree = ast.parse(src)
+lua = luadata.read("TalentExtractor.lua", encoding="utf-8")
 specs: list[SpecData] = []
 
-for field in tree.body.body[0].values[0].fields:
-	specs.append(parse_spec_data(field.value.fields))
+for spec in lua:
+	specs.append(parse_spec_data(lua[spec]))
 
 specs.sort(key=get_build, reverse=True)
-build = specs[0].build
+build = specs[0].lastUpdateBuild
 
 specs.sort(key=get_spec_id)
 output = generate_lua_table(build, specs)
